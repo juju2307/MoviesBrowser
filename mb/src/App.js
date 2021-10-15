d00e129aedb40da0ea16b8701cbdc79b9433b939
juju2307 <metzjulie2@gmail.com>
2021-10-15 17:11:19 +0200

    random and carousel

--- mb/src/App.js
+++ mb/src/App.js
@@ -10,19 +10,21 @@ import Nav from "./components/Nav.jsx";
 
 
 export default function App() {
-        
+      
+    
   const [trending, setTrending] = useState([]);
   const fetchMovies = async ( ) => {
      try {
            const datatrending = await fetch(
-           'https://api.themoviedb.org/3/movie/popular?api_key=95ef57a6064864cfbc9afc899cc5119f&page=1'
+           'https://api.themoviedb.org/3/trending/movie/day?api_key=95ef57a6064864cfbc9afc899cc5119f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate'
            );
-           const data = await datatrending.json();
-           setTrending(data);
-           console.log(data);
+           const trending= await datatrending.json();
+
+           setTrending(trending);
+           console.log(trending);
+
            } catch(err) {
               console.error(err);
-            
            }
   }
 
@@ -31,18 +33,15 @@ export default function App() {
       
   }, []);
 
-
-  
       return(
-        
           <Router>
               <div>
-                 
-                  <Nav/>
+                 <Nav/>
+
                   <Switch>
-                      <Route exact path="/" component={Home}/>
-                      <Route path="/discover" component={Discover}/>
-                      <Route path="/details" component={Details}/>
+                      <Route exact path="/"><Home trending={trending}/></Route>
+                      <Route path="/discover"><Discover/></Route>
+                      <Route path="/details"><Details/></Route>
 
                   </Switch>
               </div>
